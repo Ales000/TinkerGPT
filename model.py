5eb892e0bf3fa918a1301e76c7ba2ff2d9ebe657
Ales000 <hdjdddhhe@gmail.com>
2025-08-31 11:12:33 +0300

    Update model.py

--- model.py
+++ model.py
@@ -147,12 +147,12 @@ class EncoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
     # Pre-LN
     def forward(self, x, mask):
-    x_norm = self.norm1(x)
-    x = x + self.dropout(self.attn(x_norm, x_norm, x_norm, mask))
+        x_norm = self.norm1(x)
+        x = x + self.dropout(self.attn(x_norm, x_norm, x_norm, mask))
     
-    x_norm = self.norm2(x)
-    x = x + self.dropout(self.ff(x_norm))
-    return x
+        x_norm = self.norm2(x)
+        x = x + self.dropout(self.ff(x_norm))
+        return x
 
 class DecoderLayer(nn.Module):
     def __init__(self, d_model, num_heads, d_ff, dropout_rate=0.1):
@@ -166,15 +166,15 @@ class DecoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
     # Pre-LN
     def forward(self, x, enc_output, src_mask, tgt_mask):
-    x_norm = self.norm1(x)
-    x = x + self.dropout(self.attn(x_norm, x_norm, x_norm, tgt_mask))
+        x_norm = self.norm1(x)
+        x = x + self.dropout(self.attn(x_norm, x_norm, x_norm, tgt_mask))
     
-    x_norm = self.norm2(x)
-    x = x + self.dropout(self.cross_attn(x_norm, enc_output, enc_output, src_mask))
+        x_norm = self.norm2(x)
+        x = x + self.dropout(self.cross_attn(x_norm, enc_output, enc_output, src_mask))
     
-    x_norm = self.norm3(x)
-    x = x + self.dropout(self.ff(x_norm))
-    return x
+        x_norm = self.norm3(x)
+        x = x + self.dropout(self.ff(x_norm))
+        return x
 
 class Transformer(nn.Module):
     def __init__(self, vocab_size, d_model, num_heads, num_layers, d_ff, pad_id, dropout_rate=0.1):
